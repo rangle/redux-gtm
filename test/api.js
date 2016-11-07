1184cd0ef1a2ded8b76fb2eaf3b30c379826fd7f
ttmarek <ttmarek@outlook.com>
2016-11-07 09:26:12 -0500

    Expose eventHelpers in the ReduxGTM API

---
+++ test/api.js
@@ -0,0 +1,18 @@
+const {
+  createMiddleware,
+  createMetaReducer,
+  eventHelpers: {
+    createGAevent,
+    createGApageview,
+  },
+} = require('../lib');
+
+describe('API', () => {
+  it('Exposes ReduxGTM correctly', () => {
+    expect(createMiddleware).toBeDefined();
+    expect(createMetaReducer).toBeDefined();
+    // Event helpers
+    expect(createGAevent).toBeDefined();
+    expect(createGApageview).toBeDefined();
+  });
+});
