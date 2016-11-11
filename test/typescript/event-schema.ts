431f8aabf7db2fee1e059c267afe2ef73414b9bf
Thomas Marek <ttmarek@outlook.com>
2016-11-11 14:26:56 -0500

    Update typings for EventDefinitionsMap (#43)

---
+++ test/typescript/event-schema.ts
@@ -0,0 +1,10 @@
+import { EventSchema } from '../../index.d';
+
+const eventSchema: EventSchema = {
+    eventProp1() {
+        return true;
+    },
+    eventProp2() {
+        return false;
+    },
+};
