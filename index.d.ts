ae3a865c5bb0c64b94cf004608d478f76550d5be
ttmarek <ttmarek@outlook.com>
2016-11-11 14:15:27 -0500

    Update typings comments

--- index.d.ts
+++ index.d.ts
@@ -10,27 +10,29 @@ export interface EventSchema {
  *
  * eventName
  * Use this property to specify the name of the event you want to emit
- * for the associated Redux action. If not provided, the event name
- * defaults to the Redux action type.
+ * for the associated action. If not provided, the event name defaults
+ * to the action type.
  *
  * eventFields
  * Attach a function to this property to define any variables you
  * would like to emit with the event. Any function assigned to this
- * property will receive the state of the application, and the
- * associated action object. Any properties in the object returned by
- * the attached function will be emitted along with the event.
+ * property will receive the state of the application (before the
+ * action), and the associated action object. Any property named
+ * "event" in the returned object will override any defaults or any
+ * event names defined in `eventName`.
  *
  * eventSchema
  * Use this property to define a schema for the event. Attach
  * validation functions for each property in the event that you want
- * to validate. If any of these validation functions return false, the
- * event will not be emitted.
+ * to validate. If any of these validation functions return false,
+ * ReduxGTM will not emit the event.
  */
 export type EventDefinition = { eventName: string } | { eventFields(prevState: any, action: any): any } | { eventSchema: EventSchema };
 
 /**
- * A map between your Redux actions and your analytics events.  Each
- * key must be a Redux action type.
+ * A map between your actions and your analytics events.  Each key
+ * must be an action type. Each property must be a valid
+ * EventDefinition or an array of EventDefinitions.
  */
 export interface EventDefinitionsMap {
   [key: string]: EventDefinition | Array<EventDefinition>;
