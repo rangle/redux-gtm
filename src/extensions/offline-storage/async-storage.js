29969859c76e1f3c0b4c1760ca4c6eef568f7b7b
ttmarek <ttmarek@outlook.com>
2016-11-30 11:23:23 -0500

    Write offline storage extension for React Native

--- src/extensions/offline-storage/async-storage.js
+++ src/extensions/offline-storage/async-storage.js
@@ -1,8 +1,26 @@
-function asyncStorage(undefined, isConnected) {
+const STORE_KEY = 'EventsStore';
+
+function asyncStorage(AsyncStorage, isConnected) {
+  const saveEvents = events =>
+    AsyncStorage.getItem(STORE_KEY)
+                .then(JSON.parse)
+                .then(oldEvents => {
+                  if (oldEvents) {
+                    return oldEvents.concat(events);
+                  }
+                  return events;
+                })
+                .then(JSON.stringify)
+                .then(AsyncStorage.setItem.bind(null, STORE_KEY))
+                .then(() => events);
+
+  const purgeEvents = () =>
+    AsyncStorage.getItem(STORE_KEY, () => AsyncStorage.removeItem(STORE_KEY))
+                .then(JSON.parse);
   return {
-    saveEvents: () => {},
-    purgeEvents: () => {},
     isConnected,
+    saveEvents,
+    purgeEvents,
   };
 }
 
